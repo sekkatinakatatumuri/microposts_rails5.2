d8e039af42b0c56531e1b289e72e4d4c27bdda49
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-08-23 08:27:49 +0000

    add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,30 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      ## ここ
+      flash[:success] = 'ユーザを登録しました。'
+      # 処理を users#show のアクションへと強制的に移動
+      redirect_to @user
+    else
+      ## ここ
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      # 単に users/new.html.erb を表示
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    # password から暗号化され password_digest として保存
+    # password_confirmation は password の確認のために使用
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
