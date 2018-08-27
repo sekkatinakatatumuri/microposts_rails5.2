968057658bbf4adffd18a93eaf477696260b8fff
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-08-27 02:26:43 +0000

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,23 @@ Rails.application.routes.draw do
   # index, show, new, create のみを実装し、edit, update, destroy は実装しない
   # つまりユーザ登録(newはsignupにしている)、一覧表示、詳細表示は可能でも、
   # ユーザ情報の編集（名前やメール、パスワードの変更など）、ユーザの削除（退会）は不可
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create] do
+    # ユーザを :id によって特定する必要があるページかどうかで、
+    # member か collection かを選択
+    member do
+      # /users/:id/followings という URL が生成
+      get :followings
+      get :followers
+    end
+    # /users/search のように :id が含まれない URL を生成
+    # collection do
+    #   get :search
+    # end
+  end
   
   # マイクロポストは作成と削除だけ実装
   resources :microposts, only: [:create, :destroy]
+  
+  # フォロー/アンフォローは作成と削除だけ実装
+  resources :relationships, only: [:create, :destroy]
 end
